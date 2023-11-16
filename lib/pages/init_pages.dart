2dcba023abdaad7199bbc66821d72fd4dd70a8bb
kathyLopezSMOKE <Katherine Lopez>
2023-11-16 01:17:23 -0500

    tercer

--- lib/pages/init_pages.dart
+++ lib/pages/init_pages.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
 
-class InitPage extends StatelessWidget {
+class InitPage extends StatefulWidget {
   const InitPage({Key? key}): super(key: key);
 
   @override
+  State<InitPage> createState() => _InitPageState();
+}
+
+class _InitPageState extends State<InitPage> {
+
+  int _currentIndex=0;
+
+  List<Widget> _pages=[
+    Center(child: Text("Principal"),),
+    Center(child: Text("Short"),),
+    Center(child: Text("Agregar"),),
+    Center(child: Text("Suscripcion"),),
+    Center(child: Text("Biblioteca"),),
+  ];
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xff212121),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        selectedFontSize: 12.0,
+        unselectedFontSize: 12.0,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: 0,
+        onTap: (int value){
+          _currentIndex=value;
+          setState(() {});
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: "Principal",
+            icon: Icon(Icons.home_filled),
+          ),
+          BottomNavigationBarItem(
+            label: "Shorts",
+            icon: Icon(Icons.play_arrow_rounded),
+          ), 
+          BottomNavigationBarItem(
+            label: "",
+            icon: Container(
+              margin: const EdgeInsets.only(top: 4.0),
+              child: const Icon(
+                Icons.add_circle_outline,
+                size: 30.0,
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Suscripciones",
+            icon: Icon(Icons.subscript_rounded),
+          ),
+          BottomNavigationBarItem(
+            label: "Biblioteca",
+            icon: Icon(Icons.video_call_rounded),
+          ),         
+        ],
+      ),
+    );
   }
 }
