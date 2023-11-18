6279c26bda91ac6f6848ec4f0ada990658db320b
kathyLopezSMOKE <Katherine Lopez>
2023-11-18 17:59:07 -0500

    api_services

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,22 @@
 import 'dart:convert';
 
+import 'package:flutter_you_app/models/video_model.dart';
 import 'package:http/http.dart' as http;
 
 class APIService{
-  getVideos() async {
+  Future<List<VideoModel>> getVideos() async {
+    List<VideoModel> videosModel = [];
+
     String _path="https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAnAeGy5nWz-mktznmZY_OU_yOS80BxtY8&maxResults=20&regionCode=PE";
     Uri _uri=Uri.parse(_path);
 
     http.Response response = await http.get(_uri);
     if(response.statusCode ==200){
       Map<String, dynamic> myMap =json.decode(response.body);
+      List videos= myMap["items"];
+      videosModel= videos.map((e)=> VideoModel.fromJson(e)).toList();
+      return videosModel;
     }
+    return videosModel;
   }
 }
